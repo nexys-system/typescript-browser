3625617071697a0ca4b4c77880cbb36ebbfa5913
Johan <johan@macair2020>
2022-08-07 00:11:02 +0200

    init

--- src/config.ts
+++ src/config.ts
@@ -1,12 +1,10 @@
-export const title = "Vite Boilerplate";
+export const title = "Typescript in Browser";
 
 export const version: string = import.meta.env.VITE_VERSION || "unset_version";
 export const sha: string = import.meta.env.VITE_GIT_SHA || "unset_sha";
 export const basename = import.meta.env.BASE_URL;
 
-const { env } = import.meta;
-
-const ghUrl = "https://github.com/nexys-system/boilerplate_vite";
+const ghUrl = "https://github.com/nexys-system/typescript-browser";
 
 export const github = {
   sha: `${ghUrl}/commit/${sha}`,
--- src/home.tsx
+++ src/home.tsx
@@ -1,14 +1,52 @@
-import { title, github } from "./config";
+import React from "react";
 
-export default () => (
-  <>
-    <h1>{title}</h1>
+import * as TS from "typescript";
 
-    <p>
-      <a href={github.url}>
-        <i className="fa fa-code"></i> Source
-      </a>
-      &nbsp;available under MIT license. Contributions are welcome.
-    </p>
-  </>
-);
+const defaultCodeString = "() =>  'ba'";
+
+export default () => {
+  const [value, setValue] = React.useState<string>(defaultCodeString);
+  const [error, setError] = React.useState<string | undefined>();
+  const [fx, setFx] = React.useState<((x: string) => string) | undefined>();
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    try {
+      const execKey = "exec";
+      const prepareCodeString = `({${execKey}: ${value}})`;
+      const c = TS.transpile(prepareCodeString);
+      console.log(c);
+      const t = eval(c);
+
+      const fx = t[execKey];
+
+      // console.log(fx());
+      // console.log(typeof fx);
+
+      const f2: (x: string) => string = (x: string) => "bac" + x;
+
+      setFx(() => fx);
+    } catch (e) {
+      console.log((e as Error).message);
+      setError((e as Error).message);
+    }
+  };
+
+  if (fx) {
+    return (
+      <>
+        <pre>{fx("ghj")}</pre>
+        <button onClick={() => setFx(undefined)}>back</button>
+      </>
+    );
+  }
+
+  return (
+    <form onSubmit={handleSubmit}>
+      {error && <p className="alert alert-danger">{error}</p>}
+
+      <textarea value={value} onChange={(e) => setValue(e.target.value)} />
+      <button type="submit">Submit</button>
+    </form>
+  );
+};
